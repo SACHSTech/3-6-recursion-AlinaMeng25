5bf6f0899096a44a323700339a536211a5d5e4f5
AlinaMeng25 <alinameng25@ycdsbk12.ca>
2025-05-16 13:31:29 -0400

    Finished count down

--- src/recursion/CountDownTimer.java
+++ src/recursion/CountDownTimer.java
@@ -3,6 +3,10 @@ package recursion;
 public class CountDownTimer {
     public static void countDown(int n) {
         // solution here
+        System.out.println(n);
+        if (n != 1) {
+            countDown(n-1);
+        }
     }
 
     public static void main(String[] args) {
